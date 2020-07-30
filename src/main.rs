33773e9397745642084dfe3aa38397eb8a3cc0c5
Khue Doan <khuedoan98@gmail.com>
2020-07-30 11:11:03 +0700

    Print pretty clock

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::time::SystemTime;
+extern crate chrono;
+use chrono::offset::Utc;
+use chrono::DateTime;
 
 fn main() {
     loop {
-        println!("{:?}", SystemTime::now());
+        let system_time = SystemTime::now();
+        let datetime: DateTime<Utc> = system_time.into();
+        println!("{}", datetime.format("%d/%m/%Y %T"));
     }
 }
