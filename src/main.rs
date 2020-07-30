49fe39b581abab5322f825dc46a10df9538799cf
Khue Doan <khuedoan98@gmail.com>
2020-07-30 11:45:20 +0700

    Import only sleep module from thread

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use chrono::offset::Utc;
 use chrono::DateTime;
 
 use std::time::Duration;
-use std::thread;
+use std::thread::sleep;
 
 fn main() {
     loop {
         let system_time = SystemTime::now();
         let datetime: DateTime<Utc> = system_time.into();
         println!("{}", datetime.format("%R - %a, %b %d"));
-        thread::sleep(Duration::from_secs(1));
+        sleep(Duration::from_secs(1));
     }
 }
