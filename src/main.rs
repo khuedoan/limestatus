0ff872f677b5d6ae4aa0298e7b524a4785342cfd
Khue Doan <khuedoan98@gmail.com>
2020-07-30 11:42:36 +0700

    Only refresh time every 1 second

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ extern crate chrono;
 use chrono::offset::Utc;
 use chrono::DateTime;
 
+use std::time::Duration;
+use std::thread;
+
 fn main() {
     loop {
         let system_time = SystemTime::now();
         let datetime: DateTime<Utc> = system_time.into();
         println!("{}", datetime.format("%R - %a, %b %d"));
+        thread::sleep(Duration::from_secs(1));
     }
 }
