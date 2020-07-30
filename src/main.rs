00d741957f79482a847b7a135c9906c38f0a47f9
Khue Doan <khuedoan98@gmail.com>
2020-07-30 19:05:09 +0700

    Prototype workspace number

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,21 @@ use std::thread::sleep;
 extern crate systemstat;
 use systemstat::{System, Platform};
 
+use std::process::Command;
+
+fn desktop() -> String {
+    let output = Command::new("/usr/bin/bspc")
+                     .arg("query")
+                     .arg("-D")
+                     .arg("-d")
+                     .arg("focused")
+                     .arg("--names")
+                     .output()
+                     .expect("failed to execute process");
+
+    return String::from_utf8_lossy(&output.stdout).trim().to_string();
+}
+
 fn battery() -> i32 {
     let sys = System::new();
 
@@ -24,7 +39,11 @@ fn main() {
     loop {
         let system_time = SystemTime::now();
         let datetime: DateTime<Utc> = system_time.into();
-        println!(" %{{c}}{} %{{r}} {:?}% ", datetime.format("%R - %a, %b %d"), battery());
+        println!(" {} %{{c}}{} %{{r}} {:?}% ",
+            desktop(),
+            datetime.format("%R - %a, %b %d"),
+            battery()
+        );
         sleep(Duration::from_secs(1));
     }
 }
