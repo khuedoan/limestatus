9c2b648b2f309982d644a8306b6d5bef132af7c6
Khue Doan <khuedoan98@gmail.com>
2020-08-01 15:31:20 +0700

    Format code

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,23 @@ extern crate chrono;
 use chrono::offset::Utc;
 use chrono::DateTime;
 
-use std::time::Duration;
 use std::thread::sleep;
+use std::time::Duration;
 
 extern crate systemstat;
-use systemstat::{System, Platform};
+use systemstat::{Platform, System};
 
 use std::process::Command;
 
 fn desktop() -> String {
     let output = Command::new("/usr/bin/bspc")
-                     .arg("query")
-                     .arg("-D")
-                     .arg("-d")
-                     .arg("focused")
-                     .arg("--names")
-                     .output()
-                     .expect("failed to execute process");
+        .arg("query")
+        .arg("-D")
+        .arg("-d")
+        .arg("focused")
+        .arg("--names")
+        .output()
+        .expect("failed to execute process");
 
     return String::from_utf8_lossy(&output.stdout).trim().to_string();
 }
@@ -28,10 +28,8 @@ fn battery() -> i32 {
     let sys = System::new();
 
     match sys.battery_life() {
-        Ok(battery) =>
-            (battery.remaining_capacity * 100.0) as i32,
-        Err(_) =>
-            0
+        Ok(battery) => (battery.remaining_capacity * 100.0) as i32,
+        Err(_) => 0,
     }
 }
 
@@ -39,7 +37,8 @@ fn main() {
     loop {
         let system_time = SystemTime::now();
         let datetime: DateTime<Utc> = system_time.into();
-        println!(" {} %{{c}}{} %{{r}} {:?}% ",
+        println!(
+            " {} %{{c}}{} %{{r}} {:?}% ",
             desktop(),
             datetime.format("%R - %a, %b %d"),
             battery()
